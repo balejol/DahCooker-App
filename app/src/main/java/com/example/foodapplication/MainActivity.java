8d99dff2ed41d9cd37bb5b6f211bfd494973296f
Jolanta <baleviciutejolanta@gmail.com>
2024-03-04 00:58:57 +0200

    Login + Login Design + Add Recipes

--- app/src/main/java/com/example/foodapplication/MainActivity.java
+++ app/src/main/java/com/example/foodapplication/MainActivity.java
@@ -1,13 +1,27 @@
 package com.example.foodapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button button2;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button2 = findViewById(R.id.button2);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, RecipesPage.class);
+                startActivity(intent);
+            }
+        });
     }
 }
